83017b14eca0814d277b2828555d001487783f61
xieyichao <1063953712@qq.com>
2019-11-18 17:16:20 +0800

    Test

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Description:
+ *
+ * @author: xieyichao
+ * CreateTime: 2019/11/18 17:13
+ */
+public class Test {
+    public static void TestToString(String s){
+        if("".equals(s)){
+            s = "Hello Xiezi";
+        }
+        System.out.println(s);
+    }
+}
